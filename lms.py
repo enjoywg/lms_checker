973e6ac6515f6922a29241139856ea209826986c
enjoyw <enjoywg@gmail.com>
2022-11-19 11:41:23 +0300

    add price and profit to message, add LEG_ZERO_PRICE

--- lms.py
+++ lms.py
@@ -10,7 +10,8 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 EXPECTED_MONTH_PROFIT = 8
-ZERO_PRICE = 3
+EPIC_ZERO_PRICE = 3
+LEG_ZERO_PRICE = 35
 EPIC_DAILY_INCOME = 0.5
 LEG_DAILY_INCOME = 1.25
 PRICE_MAX_EPIC = 30
@@ -28,8 +29,10 @@ def get_data(page, rarity, price_max):
     return res_dict.get('items')
 
 
-def send_message(bot, message):
+def send_message(message):
     """Send message to tg"""
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
     try:
         bot.send_message(
             chat_id=TELEGRAM_CHAT_ID,
@@ -50,6 +53,7 @@ def get_all_data(rarity, price_max):
         sleep(1)
 
     return res
+
 
 def check_and_save_good_nft(good_nft):
     with open('lms.txt', 'r') as f:
@@ -72,20 +76,23 @@ def check_and_save_good_nft(good_nft):
 def get_good_nft(nfts, rarity):
     good_nft = []
     for nft in nfts:
-        visaLeft = nft.get('properties').get('visaLeft')
+        visa_left = nft.get('properties').get('visaLeft')
         price = nft.get('price')
         if rarity == 4:
             daily_income = EPIC_DAILY_INCOME
+            zero_price = EPIC_ZERO_PRICE
         elif rarity == 5:
             daily_income = LEG_DAILY_INCOME
+            zero_price = LEG_ZERO_PRICE
         else:
             raise ValueError('Invalid value: rarity')
-        profit = visaLeft * daily_income + ZERO_PRICE - price
+        profit = visa_left * daily_income + zero_price - price
         roi = round(profit / price * 100, 2)
-        month_profit = round(30 * profit / visaLeft, 2)
+        month_profit = round(30 * profit / visa_left, 2)
         if month_profit >= EXPECTED_MONTH_PROFIT:
             good_nft.append({
                 'link': 'https://market.letmespeak.org/escrow/' + nft.get('id'),
+                'price': price, 'profit': profit,
                 'roi': roi, 'month_profit': month_profit
             })
 
@@ -110,13 +117,12 @@ def main():
     good_nft_leg = get_good_nft(nfts_leg, rarity=5)
 
     if good_nft_epic or good_nft_leg:
-        bot = telegram.Bot(token=TELEGRAM_TOKEN)
         message = (
             f'Something interesting has come up\n\n'
             f'Epic:\n{good_nft_epic}\n\n'
             f'Legendary:\n{good_nft_leg}'
         )
-        send_message(bot, message)
+        send_message(message)
 
 
 if __name__ == '__main__':
